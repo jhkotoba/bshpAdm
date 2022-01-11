873c023180899478722bc8cdf47ed59ca7dd3de4
JeHoon <JeHoon@DESKTOP-0NU9ABL>
2022-01-11 23:32:54 +0900

    fetchUtil 추가

---
+++ src/main/resources/static/script/common/fetchUtil.js
@@ -0,0 +1,43 @@
+export const fetchUtil = {
+
+	get: url => {
+		return new Promise((resolve, reject) => {
+			fetch(url, {
+				method: 'GET',				
+				mode: 'cors',
+				cache: 'default',
+				credentials: 'same-origin',
+				headers: {
+            		'Content-Type': 'application/json',
+        		},
+        		redirect: 'follow',
+        		referrer: 'no-referrer',
+			})
+			.then(response => response.json())
+			.then(data => resolve(data))
+			.catch(error => reject(error))
+			.finally(() => console.log());
+		})
+	},
+	
+	post: param => {
+		return new Promise((resolve, reject) => {
+			fetch(param.url, {
+				method: 'POST',				
+				mode: 'cors',
+				cache: 'default',
+				credentials: 'same-origin',
+				headers: {
+            		'Content-Type': 'application/json',
+        		},
+        		redirect: 'follow',
+        		referrer: 'no-referrer',
+        		body: JSON.stringify(param.body)
+			})
+			.then(response => response.json())
+			.then(data => resolve(data))
+			.catch(error => reject(error))
+			.finally(() => console.log());
+		})
+	}
+}
