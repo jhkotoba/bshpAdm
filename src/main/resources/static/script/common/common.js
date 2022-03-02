234e2f06c11ed2c5a0101bf49cf9ba6596b1c715
JeHoon <JeHoon@DESKTOP-0NU9ABL>
2022-03-02 23:36:08 +0900

    로그인 오류수정

--- src/main/resources/static/script/common/common.js
+++ src/main/resources/static/script/common/common.js
@@ -1,9 +1,9 @@
-import { commonConstant } from "./commonConstant.js";
+import { constant } from "./constant.js";
 import { stringUtil } from "./stringUtil.js";
 import { dateUtil } from "./dateUtil.js";
 import { fetchUtil } from "./fetchUtil.js";
 
-window.commonConstant = commonConstant;
+window.constant = constant;
 window.stringUtil = stringUtil;
 window.dateUtil = dateUtil;
 window.fetchUtil = fetchUtil;
