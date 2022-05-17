9b4b5f66c81d64be4ee08defa353307238fadafb
JeHoon <JeHoon@DESKTOP-0NU9ABL>
2022-05-17 23:42:38 +0900

    회원신청 - 핸드폰번호 입력 추가

--- src/main/resources/static/script/common/stringUtil.js
+++ src/main/resources/static/script/common/stringUtil.js
@@ -1,4 +1,15 @@
-export const stringUtil = {
-
-
+/** 전화번호 포맷
+ */
+export const phoneFormat = (number) => {
+	if(number){
+		number = String(number);
+		if(number.length > 13){
+			return number;
+		}else{
+			return number.replace(/[^0-9]/, '')
+				.replace(/^(\d{2,3})(\d{3,4})(\d{4})$/, `$1-$2-$3`);	
+		}
+	}else{
+		return '';
+	}
 }
--- src/main/resources/static/script/login/join.js
+++ src/main/resources/static/script/login/join.js
@@ -1,24 +1,28 @@
 import { constant } from "/static/script/common/constant.js";
+import { phoneFormat } from "/static/script/common/stringUtil.js";
 import { postFetch } from "/static/script/common/fetchUtil.js";
 
 //암호화 객체 생성
 const crypt = new JSEncrypt();
-window.addEventListener('DOMContentLoaded', function(event){
+window.addEventListener('DOMContentLoaded', function(){
 	
 	// 암호화 공개키 세팅
 	crypt.setPublicKey(constant.aes256.publicKey);
 	
 	// 취소
-	cancel.addEventListener('click', e => window.history.back());
+	cancel.addEventListener('click', () => window.history.back());
 	
 	// 가입신청
 	request.addEventListener('click', joinRequest);
+	
+	// 핸드폰번호 포멧
+	phone.addEventListener('keypress', e => e.target.value = phoneFormat(e.target.value));
 });
 
 /**
  * 가입신청
  */
-function joinRequest(event){
+function joinRequest(){
 	
 	let adminId = crypt.encrypt(document.getElementById("adminId").value);
 	let password = crypt.encrypt(document.getElementById("passwd").value);
